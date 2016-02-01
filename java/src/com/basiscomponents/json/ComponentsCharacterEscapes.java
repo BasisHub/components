c8145cf0e8ed0654bfa5bc7f9df7c19d8a3ecddc
Stephan Wald <swald@basis.com>
2016-02-01 14:22:44 +0100

    json escaping rules

---
+++ java/src/com/basiscomponents/json/ComponentsCharacterEscapes.java
@@ -0,0 +1,45 @@
+package com.basiscomponents.json;
+
+import org.apache.commons.lang.StringEscapeUtils;
+
+import com.fasterxml.jackson.core.SerializableString;
+import com.fasterxml.jackson.core.io.CharacterEscapes;
+import com.fasterxml.jackson.core.io.SerializedString;
+
+public class ComponentsCharacterEscapes extends CharacterEscapes
+{
+    private final int[] asciiEscapes;
+    
+    public ComponentsCharacterEscapes()
+    {
+        // start with set of characters known to require escaping (double-quote, backslash etc)
+        int[] esc = new int[256];
+        int[] stdesc = CharacterEscapes.standardAsciiEscapesForJSON();
+        for (int i=0; i< stdesc.length-1; i++){
+        	esc[i]=stdesc[i];
+        }
+        for (int i=stdesc.length; i<esc.length-1; i++){
+        	esc[i]=CharacterEscapes.ESCAPE_STANDARD;;
+        }
+
+        // and force escaping of a few others:
+        esc['<'] = CharacterEscapes.ESCAPE_STANDARD;
+        esc['>'] = CharacterEscapes.ESCAPE_STANDARD;
+        esc['&'] = CharacterEscapes.ESCAPE_STANDARD;
+        esc['\''] = CharacterEscapes.ESCAPE_STANDARD;
+        esc['ä'] = CharacterEscapes.ESCAPE_STANDARD;
+        asciiEscapes = esc;
+        
+    }
+    // this method gets called for character codes 0 - 127
+    @Override public int[] getEscapeCodesForAscii() {
+        return asciiEscapes;
+    }
+    // and this for others; we don't need anything special here
+    @Override public SerializableString getEscapeSequence(int ch) {
+    	String s = new String();
+    	s+=(char) ch;
+    	return  new SerializedString(StringEscapeUtils.escapeJavaScript(s));
+        //return null;
+    }
+}
