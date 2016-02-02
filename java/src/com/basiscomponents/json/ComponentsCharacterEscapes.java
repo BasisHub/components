eace056e7c35501677fb0212cb3830d6e7478b0b
Stephan Wald <swald@basis.com>
2016-02-02 09:12:37 +0100

    escape euro sign to unicode

--- java/src/com/basiscomponents/json/ComponentsCharacterEscapes.java
+++ java/src/com/basiscomponents/json/ComponentsCharacterEscapes.java
@@ -13,14 +13,12 @@ public class ComponentsCharacterEscapes extends CharacterEscapes
     public ComponentsCharacterEscapes()
     {
         // start with set of characters known to require escaping (double-quote, backslash etc)
-        int[] esc = new int[256];
+        int[] esc = new int[128];
         int[] stdesc = CharacterEscapes.standardAsciiEscapesForJSON();
         for (int i=0; i< stdesc.length-1; i++){
         	esc[i]=stdesc[i];
         }
-        for (int i=stdesc.length; i<esc.length-1; i++){
-        	esc[i]=CharacterEscapes.ESCAPE_STANDARD;;
-        }
+
 
         // and force escaping of a few others:
         esc['<'] = CharacterEscapes.ESCAPE_STANDARD;
@@ -36,6 +34,9 @@ public class ComponentsCharacterEscapes extends CharacterEscapes
     }
     // and this for others; we don't need anything special here
     @Override public SerializableString getEscapeSequence(int ch) {
+    	if (ch==172){
+    		return new SerializedString("\\u20AC"); 
+    	}
     	String s = new String();
     	s+=(char) ch;
     	return  new SerializedString(StringEscapeUtils.escapeJavaScript(s));
