731868bd77b0cb33f21dea50cdcc3af54b709e2b
Stephan Wald <swald@basis.com>
2016-02-02 09:12:21 +0100

    trim strings for json

--- java/src/com/basiscomponents/db/ResultSet.java
+++ java/src/com/basiscomponents/db/ResultSet.java
@@ -813,7 +813,7 @@ public class ResultSet implements java.io.Serializable {
 						case java.sql.Types.NCHAR                   :
 						case java.sql.Types.LONGVARCHAR             :
 						case java.sql.Types.LONGNVARCHAR            :
-							String s =  dr.getField(fn).getString();
+							String s =  dr.getField(fn).getString().trim();
 //							s=StringEscapeUtils.escapeJavaScript(s);
 							g.writeStringField(fn,s);
 							break;
