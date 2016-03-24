b18fda7a7f440c7f2e12ed74ac4fa3d9ba2bb7d0
Stephan Wald <swald@basis.com>
2016-03-24 19:45:11 +0100

    Build a ResultSet directly from JDBC giving an SQL statement

---
+++ java/src/com/basiscomponents/db/JDBCDataSource.java
@@ -0,0 +1,35 @@
+package com.basiscomponents.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JDBCDataSource {
+	
+	private String JDBCDriver;
+	private String URL;
+	private String User;
+	private String Password;
+	
+	@SuppressWarnings("unused")
+	private JDBCDataSource(){};
+	
+	public JDBCDataSource(String JDBCDriver, String URL, String User, String Password) throws ClassNotFoundException{
+		this.JDBCDriver 	= JDBCDriver;
+		this.URL 			= URL;
+		this.User			= User;
+		this.Password		= Password;
+		
+		Class.forName(JDBCDriver);
+	}
+	
+	public ResultSet fetchResultSet(String sql) throws SQLException{
+		  Connection conn = DriverManager.getConnection(URL,User,Password);
+	      Statement stmt = conn.createStatement();
+	      java.sql.ResultSet rs = stmt.executeQuery(sql);
+	      ResultSet r = new ResultSet(rs); 
+	      return r;
+	}
+
+}
