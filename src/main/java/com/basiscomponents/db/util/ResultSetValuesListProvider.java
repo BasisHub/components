b4f96a427bde1ceab103001214e5ccb743392e0c
David Amore <damore@basis.com>
2018-03-29 13:04:04 +0200

    added functionality to return FieldValues as List

---
+++ src/main/java/com/basiscomponents/db/util/ResultSetValuesListProvider.java
@@ -0,0 +1,28 @@
+package com.basiscomponents.db.util;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import com.basiscomponents.db.ResultSet;
+
+public class ResultSetValuesListProvider {
+	private ResultSetValuesListProvider() {
+	}
+	public static List<String> getFieldValuesAsList(ResultSet rs, String columnName){
+		return getFieldValuesAsList(rs, columnName, false);
+	}
+
+	public static List<String> getFieldValuesAsList(ResultSet rs, String columnName, boolean addEmptyRow) {
+		Stream<String> values = rs.getDataRows().stream().map(dr -> dr.getAttribute(columnName));
+		if (!addEmptyRow) {
+			values = values.filter(Objects::nonNull);
+		}
+		return values.collect(Collectors.toList());
+	}
+
+	public static String getFieldValuesListAsString(List<String> list, String delimiter) {
+		return list.stream().collect(Collectors.joining(delimiter));
+	}
+}
