e3442ba69fb85ac0f4abf8086ed590c113904548
Stephan Wald <swald@basis.com>
2018-08-15 13:26:09 +0200

    added websocket server WsServer and CrossEventDispatcher classes

---
+++ src/main/java/com/basiscomponents/comm/CrossEventDispatcher.java
@@ -0,0 +1,118 @@
+package com.basiscomponents.comm;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.security.SecureRandom;
+
+import com.basis.bbj.client.datatypes.BBjVector;
+import com.basis.bbj.client.util.BBjException;
+import com.basis.bbj.proxies.BBjAPI;
+import com.basis.bbj.proxies.BBjNamespace;
+import com.basis.bbj.proxies.BBjSessionInfo;
+import com.basis.bbj.web.gwt.client.InitializationException;
+
+
+public class CrossEventDispatcher  {
+	
+	private HashMap<String,BBjAPI> InterpreterMap;
+	private HashMap<Integer,String> PIDMap;
+	protected static SecureRandom random = new SecureRandom();
+	
+	
+	public static Object getInstance(BBjAPI api)  {
+		BBjNamespace ns = api.getGlobalNamespace();
+		try {
+			Object ed = ns.getValue("bde2937e9287be9273be9273be90273b0e273b0e273b0e273b0e7");
+			return ed;
+		} catch (BBjException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		Object ed = new CrossEventDispatcher();
+		try {
+			ns.setValue("bde2937e9287be9273be9273be90273b0e273b0e273b0e273b0e7",ed);
+		} catch (BBjException e) {
+			e.printStackTrace();
+		}
+		return ed;
+	}
+	
+	private CrossEventDispatcher() {
+		InterpreterMap = new HashMap<>();
+		PIDMap = new HashMap<>();
+	}
+	
+	public synchronized String register(BBjAPI api) {
+
+		//first do a self-healing cleanup of garbage
+		cleanup(api);
+		
+        String tag= Long.toString( Math.abs( random.nextLong() ), 256 )+Long.toString( Math.abs( random.nextLong() ), 256 )+System.currentTimeMillis();
+		InterpreterMap.put(tag, api);
+		try {
+			PIDMap.put(api.getCurrentSessionInfo().getSessionID(), tag);
+		} catch (BBjException e) {
+			System.err.println("could not add session to PID Map!");
+			e.printStackTrace();
+		}
+		
+		return tag;
+	}
+	
+	public void postPriorityCustomEvent(String tag, String payload) throws Exception {
+		System.out.println("postPriorityCustomEvent");
+		
+		BBjAPI api = InterpreterMap.get(tag);
+
+		if (api != null) {
+			api.postPriorityCustomEvent(tag, payload);
+			System.out.println("posting Event to "+tag);
+		}
+	}
+	
+	public String toString() {
+		return PIDMap.size()+" sessions in map";
+	}
+	
+	public void cleanup(BBjAPI api) {
+		
+		ArrayList<Integer> pidlist = new ArrayList<>();
+		ArrayList<Integer> dellist = new ArrayList<>();
+		try {
+			BBjVector sis = api.getSessionInfos();
+			
+			Iterator it = sis.iterator();
+			while (it.hasNext())
+			{
+				BBjSessionInfo si = (BBjSessionInfo) it.next();
+				System.out.println("got "+si.getID());
+				pidlist.add(si.getID());
+			}
+			
+		} catch (BBjException e) {
+			System.err.println("Cleanup failed!");
+			e.printStackTrace();
+			return;
+		}
+		
+		Iterator<Integer> iit = PIDMap.keySet().iterator();
+		while (iit.hasNext()) {
+			Integer pid = iit.next();
+			if (!pidlist.contains(pid)) {
+				dellist.add(pid);
+			}
+		}
+		
+		iit = dellist.iterator();
+		while(iit.hasNext()) {
+			Integer pid = iit.next();
+			String tag = PIDMap.get(pid);
+			PIDMap.remove(pid);
+			InterpreterMap.remove(tag);
+		}
+
+	}
+	
+}
---
+++ src/main/java/com/basiscomponents/comm/WsServer.java
@@ -0,0 +1,248 @@
+package com.basiscomponents.comm;
+/*
+ * Copyright (c) 2010-2018 Nathan Rajlich
+ *
+ *  Permission is hereby granted, free of charge, to any person
+ *  obtaining a copy of this software and associated documentation
+ *  files (the "Software"), to deal in the Software without
+ *  restriction, including without limitation the rights to use,
+ *  copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the
+ *  Software is furnished to do so, subject to the following
+ *  conditions:
+ *
+ *  The above copyright notice and this permission notice shall be
+ *  included in all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ *  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ *  OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ *  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ *  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ *  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ *  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ *  OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
+import java.nio.ByteBuffer;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManagerFactory;
+
+import org.java_websocket.WebSocket;
+import org.java_websocket.WebSocketImpl;
+import org.java_websocket.framing.Framedata;
+import org.java_websocket.handshake.ClientHandshake;
+import org.java_websocket.server.DefaultSSLWebSocketServerFactory;
+import org.java_websocket.server.WebSocketServer;
+
+import com.basis.bbj.client.util.BBjException;
+import com.basis.bbj.proxies.BBjAPI;
+import com.basis.bbj.proxies.BBjNamespace;
+
+/**
+ * A simple WebSocketServer implementation. Keeps track of a "chatroom".
+ */
+public class WsServer extends WebSocketServer {
+	
+	private HashMap<String,ArrayList<WebSocket>> ConList;
+	private CrossEventDispatcher EDispatcher;
+
+	public WsServer( int port ) throws UnknownHostException {
+		super( new InetSocketAddress( port ) );
+		ConList = new HashMap<>();
+	}
+
+	public WsServer( InetSocketAddress address ) {
+		super( address );
+		ConList = new HashMap<>();
+	}
+
+	@Override
+	public void onOpen( WebSocket conn, ClientHandshake handshake ) {
+		registerConnection(conn);
+	}
+
+	private void registerConnection(WebSocket conn) {
+		String tag = getTag(conn.getResourceDescriptor());
+		ArrayList<WebSocket> al;
+		al = ConList.get(tag);
+		if (al==null) {
+			al = new ArrayList<>();
+			ConList.put(tag, al);
+		}
+		al.add(conn);
+		
+		System.out.println("registered: "+ConList);
+
+	}
+	
+	public void setEventDispatcher(CrossEventDispatcher ed) {
+		this.EDispatcher = ed;
+	}
+
+	@Override
+	public void onClose( WebSocket conn, int code, String reason, boolean remote ) {
+		//TODO: add a self-healing cleanup; check all connections for being closed and delete them
+		// to keep the ConLinst clean!
+	}
+
+	@Override
+	public void onMessage( WebSocket conn, String message ) {
+//		broadcast( message );
+		String tag = getTag(conn.getResourceDescriptor());
+		System.out.println( "tag "+tag +": "+ message );
+		
+		if (this.EDispatcher != null) {
+			try {
+				this.EDispatcher.postPriorityCustomEvent(tag, message);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+
+	@Override
+	public void onMessage( WebSocket conn, ByteBuffer message ) {
+//		broadcast( message.array() );
+		String tag = getTag(conn.getResourceDescriptor());
+		System.out.println( "tag "+tag +": "+ message );
+		
+		if (this.EDispatcher != null) {
+			try {
+				this.EDispatcher.postPriorityCustomEvent(tag, message.toString());
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+	
+	public void send(String tag,String message) {
+		ArrayList<WebSocket> al;
+		al = ConList.get(tag);
+		if (al!=null) {
+			Iterator<WebSocket> it = al.iterator();
+			while (it.hasNext()) {
+				WebSocket conn = it.next();
+				if (!conn.isClosed() || !conn.isClosing())
+					conn.send(message);
+			}
+		}
+		
+	}
+
+
+//	public static void main( String[] args ) throws InterruptedException , IOException {
+//		WebSocketImpl.DEBUG = false;
+//		int port = 8887; // 843 flash policy port
+//		try {
+//			port = Integer.parseInt( args[ 0 ] );
+//		} catch ( Exception ex ) {
+//		}
+//		WsServer s = new WsServer( port );
+//		s.start();
+//		System.out.println( "ChatServer started on port: " + s.getPort() );
+//
+//		BufferedReader sysin = new BufferedReader( new InputStreamReader( System.in ) );
+//		while ( true ) {
+//			String in = sysin.readLine();
+//			s.broadcast( in );
+//			if( in.equals( "exit" ) ) {
+//				s.stop(1000);
+//				break;
+//			}
+//		}
+//	}
+	
+	@Override
+	public void onError( WebSocket conn, Exception ex ) {
+		ex.printStackTrace();
+		if( conn != null ) {
+			// some errors like port binding failed may not be assignable to a specific websocket
+		}
+	}
+
+	@Override
+	public void onStart() {
+		System.out.println("Server started!");
+	}
+	
+	private String getTag(String resourceDescriptor) {
+		String tag = new String(resourceDescriptor);
+		if (tag.startsWith("/"))
+			tag=tag.substring(1);
+		
+		if (tag.contains("?"))
+			tag=tag.substring(0, tag.indexOf("?"));
+		
+		return tag;
+	}
+
+	public static Object getInstance(BBjAPI api, int port ) throws KeyStoreException, NoSuchAlgorithmException, CertificateException, FileNotFoundException, IOException, UnrecoverableKeyException, KeyManagementException  {
+		BBjNamespace ns = api.getGlobalNamespace();
+		try {
+			Object ed = ns.getValue("bdi98273bv98723bv9e72bv9e72bv9e7bv92e7b2e");
+			return ed;
+		} catch (BBjException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		WsServer ed = new WsServer(port);
+		
+		
+		try {
+			ns.setValue("bdi98273bv98723bv9e72bv9e72bv9e7bv92e7b2e",ed);
+		} catch (BBjException e) {
+			e.printStackTrace();
+		}
+		
+		// load up the key store
+		String STORETYPE = "JKS";
+		String KEYSTORE = "d:/github/Java-WebSocket/src/main/example/keystore.jks";
+		String STOREPASSWORD = "storepassword";
+		String KEYPASSWORD = "keypassword";
+
+		KeyStore ks = KeyStore.getInstance( STORETYPE );
+		File kf = new File( KEYSTORE );
+		ks.load( new FileInputStream( kf ), STOREPASSWORD.toCharArray() );
+
+		KeyManagerFactory kmf = KeyManagerFactory.getInstance( "SunX509" );
+		kmf.init( ks, KEYPASSWORD.toCharArray() );
+		TrustManagerFactory tmf = TrustManagerFactory.getInstance( "SunX509" );
+		tmf.init( ks );
+
+		SSLContext sslContext = null;
+		sslContext = SSLContext.getInstance( "TLS" );
+		sslContext.init( kmf.getKeyManagers(), tmf.getTrustManagers(), null );
+
+		ed.setWebSocketFactory( new DefaultSSLWebSocketServerFactory( sslContext ) );
+		
+		
+		ed.start();
+		return ed;
+	}
+	
+}
