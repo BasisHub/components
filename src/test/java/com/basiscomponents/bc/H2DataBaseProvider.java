bd1bf4abaccd0e622fece1be2539c6c9d4fdfb4a
Christian Bauer <christian.b1998@web.de>
2019-05-27 13:19:25 +0200

    More retrieve(sql) tests added; First retrieve() tests added;

--- src/test/java/com/basiscomponents/bc/H2DataBaseProvider.java
+++ src/test/java/com/basiscomponents/bc/H2DataBaseProvider.java
@@ -9,14 +9,77 @@ public class H2DataBaseProvider {
 
 	private static String sql;
 
-	public static void createTestDataBase() throws SQLException {
+	public static void createTestDataBaseForSQLRetrieve() throws SQLException {
 		try (
 		Connection con = DriverManager.getConnection("jdbc:h2:./src/test/testH2DataBases/test1", "sa", "sa");
 		Statement stmt = con.createStatement();
 		) {
-			sql = "CREATE TABLE IF NOT EXISTS REGISTRATION (first VARCHAR(255), age INTEGER)";
+			sql = "CREATE TABLE IF NOT EXISTS REGISTRATION (first VARCHAR(255), age INTEGER, customerID INTEGER)";
 			stmt.executeUpdate(sql);
+			sql = "insert into REGISTRATION VALUES ('Alfred', 62, 1)";
+			stmt.executeUpdate(sql);
+			sql = "insert into REGISTRATION VALUES ('Simpson', 42, 0)";
+			stmt.executeUpdate(sql);
+			sql = "CREATE TABLE IF NOT EXISTS REGISTRATION2 (first VARCHAR(255), age INTEGER, customerID INTEGER)";
+			stmt.executeUpdate(sql);
+			sql = "insert into REGISTRATION2 VALUES ('Jasper', 33, 1)";
+			stmt.executeUpdate(sql);
+			sql = "insert into REGISTRATION2 VALUES ('Alfred', 62, 1)";
+			stmt.executeUpdate(sql);
+
+			sql = "CREATE TABLE IF NOT EXISTS FULLREGISTRATION (first VARCHAR(255), age INTEGER, customerID INTEGER)";
+			stmt.executeUpdate(sql);
+			sql = "insert into FULLREGISTRATION VALUES ('Alfred', 62, 1)";
+			stmt.executeUpdate(sql);
+			sql = "insert into FULLREGISTRATION VALUES ('Simpson', 42, 0)";
+			stmt.executeUpdate(sql);
+			sql = "insert into FULLREGISTRATION VALUES ('Jasper', 33, 1)";
+			stmt.executeUpdate(sql);
+			sql = "insert into FULLREGISTRATION VALUES ('Alfred', 62, 1)";
+			stmt.executeUpdate(sql);
+
 			sql = "CREATE TABLE IF NOT EXISTS TREES (name CHAR(5), rings INT, height DOUBLE)";
+			stmt.executeUpdate(sql);
+			sql = "insert into TREES VALUES ('tree1', 155, 144.32)";
+			stmt.executeUpdate(sql);
+			sql = "insert into TREES VALUES ('tree2', 132, 1004.53)";
+			stmt.executeUpdate(sql);
+
+			sql = "CREATE TABLE IF NOT EXISTS CUSTOMERS (name CHAR(50), customerID INTEGER, country VARCHAR(255))";
+			stmt.executeUpdate(sql);
+			sql = "insert into CUSTOMERS VALUES ('Freeman', 1, 'USA')";
+			stmt.executeUpdate(sql);
+			sql = "insert into CUSTOMERS VALUES ('Jasper', 2, 'England')";
+			stmt.executeUpdate(sql);
+			sql = "insert into CUSTOMERS VALUES ('Simpson', 3, 'USA')";
+			stmt.executeUpdate(sql);
+			sql = "insert into CUSTOMERS VALUES ('Alfred', 4, 'England')";
+			stmt.executeUpdate(sql);
+			sql = "insert into CUSTOMERS VALUES ('Jenkins', 5, 'Australia')";
+			stmt.executeUpdate(sql);
+			sql = "insert into CUSTOMERS VALUES ('Caesar', 6, 'England')";
+			stmt.executeUpdate(sql);
+			sql = "insert into CUSTOMERS VALUES ('Peter', 7, 'USA')";
+			stmt.executeUpdate(sql);
+			sql = "insert into CUSTOMERS VALUES ('Flint', 8, 'USA')";
+			stmt.executeUpdate(sql);
+		}
+	}
+
+	public static void createTestDataBaseForNormalRetrieve() throws SQLException {
+		try (Connection con = DriverManager.getConnection("jdbc:h2:./src/test/testH2DataBases/test2", "sa", "sa");
+				Statement stmt = con.createStatement();) {
+
+			sql = "CREATE TABLE IF NOT EXISTS REGISTRATION (first VARCHAR(255), age INTEGER, customerID INTEGER)";
+			stmt.executeUpdate(sql);
+			sql = "insert into REGISTRATION VALUES ('Alfred', 62, 1)";
+			stmt.executeUpdate(sql);
+			sql = "insert into REGISTRATION VALUES ('Simpson', 42, 0)";
+			stmt.executeUpdate(sql);
+
+			sql = "CREATE TABLE IF NOT EXISTS BOOLTABLE (boolType BOOL, booleanType BOOLEAN, bitType BIT)";
+			stmt.executeUpdate(sql);
+			sql = "insert into BOOLTABLE VALUES (true, false, true)";
 			stmt.executeUpdate(sql);
 		}
 	}
--- src/test/java/com/basiscomponents/bc/SqlTableBCConnetionTest.java
+++ src/test/java/com/basiscomponents/bc/SqlTableBCConnetionTest.java
@@ -17,6 +17,7 @@ import com.basiscomponents.db.ResultSet;
 public class SqlTableBCConnetionTest {
 
 	private String sql;
+	private ResultSet rs;
 
 	/**
 	 * Loading the h2-Driver and creating the test databases.
@@ -30,7 +31,8 @@ public class SqlTableBCConnetionTest {
 	public static void initialize()
 			throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
 		Class.forName("org.h2.Driver").newInstance();
-		H2DataBaseProvider.createTestDataBase();
+		H2DataBaseProvider.createTestDataBaseForSQLRetrieve();
+		H2DataBaseProvider.createTestDataBaseForNormalRetrieve();
 	}
 
 	/**
@@ -57,27 +59,53 @@ public class SqlTableBCConnetionTest {
 	 * @throws Exception
 	 */
 	@Test
-	public void SqlTablBCConnectionSimpleTableTest() throws Exception {
-		try (Connection con = DriverManager.getConnection("jdbc:h2:./src/test/testH2DataBases/test1", "sa", "sa");
+	public void SqlTablBCGetDataSimpleTest() throws Exception {
+		try (Connection con = DriverManager.getConnection("jdbc:h2:./src/test/testH2DataBases/test2", "sa", "sa");
 				Statement stmt = con.createStatement();) {
-
-			// Insert new data
-			sql = "DELETE FROM REGISTRATION";
-			stmt.execute(sql);
-			sql = "insert into REGISTRATION VALUES ('Freeman', 34)";
-			stmt.executeUpdate(sql);
 
 			// Set table and get its data with normal retrieve()
 			SqlTableBC sqlTable = new SqlTableBC(con);
 			sqlTable.setTable("REGISTRATION");
-			ResultSet rs = sqlTable.retrieve();
+			rs = sqlTable.retrieve();
 
-			assertTrue(rs.getColumnCount() == 2);
+			// Checking the ColumnNames and results
+			assertTrue(rs.getColumnCount() == 3);
 			assertTrue(rs.getColumnNames().contains("FIRST"));
 			assertTrue(rs.getColumnNames().contains("AGE"));
+			assertTrue(rs.getColumnNames().contains("CUSTOMERID"));
 
-			assertEquals("Freeman", rs.get(0).getFieldValue("FIRST"));
-			assertEquals(34, rs.get(0).getFieldValue("AGE"));
+			assertEquals("Alfred", rs.get(0).getFieldValue("FIRST"));
+			assertEquals(62, rs.get(0).getFieldValue("AGE"));
+			assertEquals(1, rs.get(0).getFieldValue("CUSTOMERID"));
+		}
+	}
+
+	/**
+	 * Creates a SqlTableBC with a connection to a h2-DataBase. The active table is
+	 * switched to REGISTRATION and its values are queried with retrieve(). The
+	 * resulting ResultSet is checked to contain the right data.
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void SqlTablBCGetDataBoolTest() throws Exception {
+		try (Connection con = DriverManager.getConnection("jdbc:h2:./src/test/testH2DataBases/test2", "sa", "sa");
+				Statement stmt = con.createStatement();) {
+
+			// Set table and get its data with normal retrieve()
+			SqlTableBC sqlTable = new SqlTableBC(con);
+			sqlTable.setTable("BOOLTABLE");
+			rs = sqlTable.retrieve();
+
+			// Checking the ColumnNames and results
+			assertTrue(rs.getColumnCount() == 3);
+			assertTrue(rs.getColumnNames().contains("BOOLTYPE"));
+			assertTrue(rs.getColumnNames().contains("BOOLEANTYPE"));
+			assertTrue(rs.getColumnNames().contains("BITTYPE"));
+
+			assertEquals(true, rs.get(0).getFieldValue("BOOLTYPE"));
+			assertEquals(false, rs.get(0).getFieldValue("BOOLEANTYPE"));
+			assertEquals(true, rs.get(0).getFieldValue("BITTYPE"));
 		}
 	}
 
@@ -89,27 +117,25 @@ public class SqlTableBCConnetionTest {
 	 * @throws Exception
 	 */
 	@Test
-	public void SqlTablBCWriteSqlTest() throws Exception {
+	public void SqlTableBCGetDataWithSQLSimpleTest() throws Exception {
 		try (Connection con = DriverManager.getConnection("jdbc:h2:./src/test/testH2DataBases/test1", "sa", "sa");
 				Statement stmt = con.createStatement();) {
 
-			// Insert new data
-			sql = "DELETE FROM REGISTRATION";
-			stmt.execute(sql);
-			sql = "insert into REGISTRATION VALUES ('Simpson', 42)";
-			stmt.executeUpdate(sql);
+			SqlTableBC sqlTable = new SqlTableBC(con);
 
 			// Collect data from the SqlTableBC
-			SqlTableBC sqlTable = new SqlTableBC(con);
 			sql = "SELECT * FROM REGISTRATION";
-			ResultSet rs = sqlTable.retrieve(sql, null);
+			rs = sqlTable.retrieve(sql, null);
 
-			assertTrue(rs.getColumnCount() == 2);
+			// Checking the ColumnNames and results
+			assertTrue(rs.getColumnCount() == 3);
 			assertTrue(rs.getColumnNames().contains("FIRST"));
 			assertTrue(rs.getColumnNames().contains("AGE"));
+			assertTrue(rs.getColumnNames().contains("CUSTOMERID"));
 
-			assertEquals("Simpson", rs.get(0).getFieldValue("FIRST"));
-			assertEquals(42, rs.get(0).getFieldValue("AGE"));
+			assertEquals("Alfred", rs.get(0).getFieldValue("FIRST"));
+			assertEquals(62, rs.get(0).getFieldValue("AGE"));
+			assertEquals(1, rs.get(0).getFieldValue("CUSTOMERID"));
 		}
 	}
 
@@ -120,43 +146,202 @@ public class SqlTableBCConnetionTest {
 	 * 
 	 * @throws Exception
 	 */
-//	@Test
-	public void SqlTablBCWriteSqlTest2() throws Exception {
+	@Test
+	public void SqlTablBCGetDataWithSQLTest2() throws Exception {
 		try (Connection con = DriverManager.getConnection("jdbc:h2:./src/test/testH2DataBases/test1", "sa", "sa");
 				Statement stmt = con.createStatement();) {
 
-			// Insert new data
-			sql = "DELETE FROM REGISTRATION;";
-			stmt.execute(sql);
-			sql = "DELETE FROM TREES;";
-			stmt.execute(sql);
-			sql = "insert into REGISTRATION VALUES ('Simpson', 42)";
-			stmt.executeUpdate(sql);
-			sql = "insert into REGISTRATION VALUES ('Jasper', 33)";
-			stmt.executeUpdate(sql);
-			sql = "insert into TREES VALUES ('tree1', 155, 144.32)";
-			stmt.executeUpdate(sql);
-			sql = "insert into TREES VALUES ('tree2', 132, 1004.53)";
-			stmt.executeUpdate(sql);
+			SqlTableBC sqlTable = new SqlTableBC(con);
 
 			// Collect data from the SqlTableBC
-			SqlTableBC sqlTable = new SqlTableBC(con);
 			sql = "SELECT * FROM REGISTRATION, TREES";
-			ResultSet rs = sqlTable.retrieve(sql, null);
+			rs = sqlTable.retrieve(sql, null);
 
-			assertTrue(rs.getColumnCount() == 5);
-			System.out.println(rs.getColumnNames());
+			// Checking the ColumnNames
+			assertTrue(rs.getColumnCount() == 6);
 			assertTrue(rs.getColumnNames().contains("FIRST"));
 			assertTrue(rs.getColumnNames().contains("AGE"));
 			assertTrue(rs.getColumnNames().contains("NAME"));
 			assertTrue(rs.getColumnNames().contains("RINGS"));
 			assertTrue(rs.getColumnNames().contains("HEIGHT"));
 
-			System.out.println(rs.toJson());
-			assertEquals("Simpson", rs.get(0).getFieldValue("FIRST"));
-			assertEquals(42, rs.get(0).getFieldValue("AGE"));
+			// Checking DataRows 0 and 1
+			assertEquals("Alfred", rs.get(0).getFieldValue("FIRST"));
+			assertEquals(62, rs.get(0).getFieldValue("AGE"));
+			assertEquals("tree1", rs.get(0).getFieldValue("NAME"));
+			assertEquals(155, rs.get(0).getFieldValue("RINGS"));
+			assertEquals(144.32, rs.get(0).getFieldValue("HEIGHT"));
+
+			assertEquals("Alfred", rs.get(1).getFieldValue("FIRST"));
+			assertEquals(62, rs.get(1).getFieldValue("AGE"));
+			assertEquals("tree2", rs.get(1).getFieldValue("NAME"));
+			assertEquals(132, rs.get(1).getFieldValue("RINGS"));
+			assertEquals(1004.53, rs.get(1).getFieldValue("HEIGHT"));
+
+			// Checking DataRows 2 and 3
+			assertEquals("Simpson", rs.get(2).getFieldValue("FIRST"));
+			assertEquals(42, rs.get(2).getFieldValue("AGE"));
+			assertEquals("tree1", rs.get(2).getFieldValue("NAME"));
+			assertEquals(155, rs.get(2).getFieldValue("RINGS"));
+			assertEquals(144.32, rs.get(2).getFieldValue("HEIGHT"));
+
+			assertEquals("Simpson", rs.get(3).getFieldValue("FIRST"));
+			assertEquals(42, rs.get(3).getFieldValue("AGE"));
+			assertEquals("tree2", rs.get(3).getFieldValue("NAME"));
+			assertEquals(132, rs.get(3).getFieldValue("RINGS"));
+			assertEquals(1004.53, rs.get(3).getFieldValue("HEIGHT"));
+		}
+	}
+
+	/**
+	 * Creates a SqlTableBC with a connection to a h2-DataBase. A SQL Statement is
+	 * created and the SqlTableBC is queried with retrieve(String sql, DataRow dr).
+	 * The UNION, UNION ALL and INTERSECT operator are used in this case. The
+	 * resulting ResultSet is checked to contain the right data.
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void SqlTablBCGetDataWithSQLUnionIntersectTest() throws Exception {
+		try (Connection con = DriverManager.getConnection("jdbc:h2:./src/test/testH2DataBases/test1", "sa", "sa");
+				Statement stmt = con.createStatement();) {
+
+			SqlTableBC sqlTable = new SqlTableBC(con);
+
+			// 1.
+			// Checking the UNION operator
+			sql = "SELECT * FROM REGISTRATION UNION SELECT * FROM REGISTRATION2";
+			rs = sqlTable.retrieve(sql, null);
+
+			// Checking the ColumnNames and results
+			assertTrue(rs.getColumnCount() == 3);
+			assertTrue(rs.getColumnNames().contains("FIRST"));
+			assertTrue(rs.getColumnNames().contains("AGE"));
+
+			assertEquals("Alfred", rs.get(0).getFieldValue("FIRST"));
+			assertEquals(62, rs.get(0).getFieldValue("AGE"));
 			assertEquals("Jasper", rs.get(1).getFieldValue("FIRST"));
+			assertEquals(33, rs.get(1).getFieldValue("AGE"));
+			assertEquals("Simpson", rs.get(2).getFieldValue("FIRST"));
+			assertEquals(42, rs.get(2).getFieldValue("AGE"));
+
+			// 2.
+			// Checking the UNION ALL operator
+			sql = "SELECT * FROM REGISTRATION UNION ALL SELECT * FROM REGISTRATION2";
+			rs = sqlTable.retrieve(sql, null);
+
+			// Checking the ColumnNames and results
+			assertTrue(rs.getColumnCount() == 3);
+			assertTrue(rs.getColumnNames().contains("FIRST"));
+			assertTrue(rs.getColumnNames().contains("AGE"));
+
+			assertEquals("Alfred", rs.get(0).getFieldValue("FIRST"));
+			assertEquals(62, rs.get(0).getFieldValue("AGE"));
+			assertEquals("Simpson", rs.get(1).getFieldValue("FIRST"));
+			assertEquals(42, rs.get(1).getFieldValue("AGE"));
+			assertEquals("Jasper", rs.get(2).getFieldValue("FIRST"));
 			assertEquals(33, rs.get(2).getFieldValue("AGE"));
+			assertEquals("Alfred", rs.get(3).getFieldValue("FIRST"));
+			assertEquals(62, rs.get(3).getFieldValue("AGE"));
+
+			// 3.
+			// Checking the INTERSECT operator
+			sql = "SELECT * FROM REGISTRATION INTERSECT SELECT * FROM REGISTRATION2";
+			rs = sqlTable.retrieve(sql, null);
+
+			// Checking the ColumnNames and results
+			assertTrue(rs.getColumnCount() == 3);
+			assertTrue(rs.getColumnNames().contains("FIRST"));
+			assertTrue(rs.getColumnNames().contains("AGE"));
+
+			assertEquals("Alfred", rs.get(0).getFieldValue("FIRST"));
+			assertEquals(62, rs.get(0).getFieldValue("AGE"));
+		}
+	}
+
+	/**
+	 * Creates a SqlTableBC with a connection to a h2-DataBase. A SQL Statement is
+	 * created and the SqlTableBC is queried with retrieve(String sql, DataRow dr).
+	 * The WHERE operator is used in this case. The resulting ResultSet is checked
+	 * to contain the right data.
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void SqlTablBCGetDataWithSQLWhereTest() throws Exception {
+		try (Connection con = DriverManager.getConnection("jdbc:h2:./src/test/testH2DataBases/test1", "sa", "sa");
+				Statement stmt = con.createStatement();) {
+
+			// Collect data from the SqlTableBC
+			SqlTableBC sqlTable = new SqlTableBC(con);
+			sql = "SELECT * FROM REGISTRATION WHERE FIRST = 'Alfred' UNION ALL SELECT * FROM REGISTRATION2 WHERE FIRST = 'Alfred'";
+			rs = sqlTable.retrieve(sql, null);
+
+			// Checking the ColumnNames and results
+			assertTrue(rs.getColumnCount() == 3);
+			assertTrue(rs.getColumnNames().contains("FIRST"));
+			assertTrue(rs.getColumnNames().contains("AGE"));
+
+			assertEquals("Alfred", rs.get(0).getFieldValue("FIRST"));
+			assertEquals(62, rs.get(0).getFieldValue("AGE"));
+			assertEquals("Alfred", rs.get(1).getFieldValue("FIRST"));
+			assertEquals(62, rs.get(1).getFieldValue("AGE"));
+		}
+	}
+
+	/**
+	 * Creates a SqlTableBC with a connection to a h2-DataBase. A SQL Statement is
+	 * created and the SqlTableBC is queried with retrieve(String sql, DataRow dr).
+	 * The GROUP BY and ORDER BY operators are used in this case. The resulting
+	 * ResultSet is checked to contain the right data.
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void SqlTablBCGetDataWithSQLGroupByOrderByTest() throws Exception {
+		try (Connection con = DriverManager.getConnection("jdbc:h2:./src/test/testH2DataBases/test1", "sa", "sa");
+				Statement stmt = con.createStatement();) {
+
+			SqlTableBC sqlTable = new SqlTableBC(con);
+
+			// 1.
+			// Checking the GROUP BY operator
+			sql = "SELECT COUNT(CUSTOMERID), COUNTRY FROM CUSTOMERS GROUP BY COUNTRY";
+			rs = sqlTable.retrieve(sql, null);
+
+			// Checking the ColumnNames
+			assertTrue(rs.getColumnCount() == 2);
+			assertTrue(rs.getColumnNames().contains("COUNT(CUSTOMERID)"));
+			assertTrue(rs.getColumnNames().contains("COUNTRY"));
+
+			// Checking the values of the resulting GROUP BY
+			assertEquals("Australia", rs.get(0).getFieldValue("COUNTRY"));
+			assertEquals((long) 1, rs.get(0).getFieldValue("COUNT(CUSTOMERID)"));
+			assertEquals("England", rs.get(1).getFieldValue("COUNTRY"));
+			assertEquals((long) 3, rs.get(1).getFieldValue("COUNT(CUSTOMERID)"));
+			assertEquals("USA", rs.get(2).getFieldValue("COUNTRY"));
+			assertEquals((long) 4, rs.get(2).getFieldValue("COUNT(CUSTOMERID)"));
+
+			// 2.
+			// Checking the ORDER BY operator
+			sql = "SELECT * FROM CUSTOMERS ORDER BY COUNTRY";
+			rs = sqlTable.retrieve(sql, null);
+
+			// Checking the ColumnNames
+			assertTrue(rs.getColumnCount() == 3);
+			assertTrue(rs.getColumnNames().contains("CUSTOMERID"));
+			assertTrue(rs.getColumnNames().contains("COUNTRY"));
+			assertTrue(rs.getColumnNames().contains("NAME"));
+
+			// Checking the order of the country fields
+			assertEquals("Australia", rs.get(0).getFieldValue("COUNTRY"));
+			assertEquals("England", rs.get(1).getFieldValue("COUNTRY"));
+			assertEquals("England", rs.get(2).getFieldValue("COUNTRY"));
+			assertEquals("England", rs.get(3).getFieldValue("COUNTRY"));
+			assertEquals("USA", rs.get(4).getFieldValue("COUNTRY"));
+			assertEquals("USA", rs.get(5).getFieldValue("COUNTRY"));
+			assertEquals("USA", rs.get(6).getFieldValue("COUNTRY"));
+			assertEquals("USA", rs.get(7).getFieldValue("COUNTRY"));
 		}
 	}
 
@@ -170,6 +355,14 @@ public class SqlTableBCConnetionTest {
 		try (Connection con = DriverManager.getConnection("jdbc:h2:./src/test/testH2DataBases/test1", "sa", "sa");
 				Statement stmt = con.createStatement();) {
 			String sql = "DROP TABLE REGISTRATION";
+			stmt.executeUpdate(sql);
+			sql = "DROP TABLE REGISTRATION2";
+			stmt.executeUpdate(sql);
+			sql = "DROP TABLE TREES";
+			stmt.executeUpdate(sql);
+			sql = "DROP TABLE FULLREGISTRATION";
+			stmt.executeUpdate(sql);
+			sql = "DROP TABLE CUSTOMERS";
 			stmt.executeUpdate(sql);
 		}
 	}
