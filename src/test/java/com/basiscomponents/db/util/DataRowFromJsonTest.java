d8368162cb2efeb79a31e5317b37a5a6af0b9bd6
Christian Bauer <christian.b1998@web.de>
2019-06-18 10:04:34 +0200

    The first tests are green now

--- src/test/java/com/basiscomponents/db/util/DataRowFromJsonTest.java
+++ src/test/java/com/basiscomponents/db/util/DataRowFromJsonTest.java
@@ -14,6 +14,24 @@ public class DataRowFromJsonTest {
 	private String json;
 
 	/**
+	 * A simple conversion of a Json-String to a DataRow, by using the full format.
+	 * In other test cases this format is shortened by dropping the
+	 * "datarow"-statement at the beginning, leaving the resulting Json-string less
+	 * complex.
+	 * 
+	 * @throws IOException
+	 * @throws ParseException
+	 */
+	@Test
+	public void dataRowFromJsonFullCorrectTest() throws IOException, ParseException {
+		json = "{\"datarow\":[{\"name\":\"John\", \"age\": 31, \"city\":\"New York\"}]}";
+		DataRow dr = DataRowFromJsonProvider.fromJson(json, new DataRow());
+		assertEquals("John", dr.getFieldValue("name"));
+//		assertEquals(31, dr.getFieldValue("age")); 
+		assertEquals("New York", dr.getFieldValue("city"));
+	}
+
+	/**
 	 * A simple conversion of a Json-String to a DataRow.
 	 * 
 	 * @throws IOException
@@ -29,17 +47,18 @@ public class DataRowFromJsonTest {
 	}
 	
 	/**
-	 * A simple conversion of a Json-String to a DataRow.
+	 * A simple conversion of a Json-String to a DataRow with many types.
 	 * 
 	 * @throws IOException
 	 * @throws ParseException
 	 */
 	@Test
 	public void dataRowFromJsonManyTypesTest() throws IOException, ParseException {
-		json = "{\"truth\": true, \"age\": 31.0, \"city\":\"New York\"}";
+		json = "{\"truth\": true, \"age\": 31.0, \"city\":\"New York\", \"Number\": 42.0}";
 		DataRow dr = DataRowFromJsonProvider.fromJson(json, new DataRow());
 		assertEquals(true, dr.getFieldValue("truth"));
 		assertEquals(31.0, dr.getFieldValue("age"));
 		assertEquals("New York", dr.getFieldValue("city"));
+		assertEquals(42.0, dr.getFieldValue("Number"));
 	}
 }
