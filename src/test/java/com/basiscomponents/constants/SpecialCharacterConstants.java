2a53eb5fd00ffd9ba6fdaedf5deb81139ef44de8
David Amore <damore@basis.de>
2019-07-01 10:19:30 +0200

    changed fileencoding to utf-8

--- src/test/java/com/basiscomponents/constants/SpecialCharacterConstants.java
+++ src/test/java/com/basiscomponents/constants/SpecialCharacterConstants.java
@@ -2,11 +2,11 @@ package com.basiscomponents.constants;
 
 public class SpecialCharacterConstants {
 
-	public static final String GERMAN_SPECIAL_CHARACTERS = "ÄäÖöÜüß";
+	public static final String GERMAN_SPECIAL_CHARACTERS = "Ã„Ã¤Ã–Ã¶ÃœÃ¼ÃŸ";
 	
-	public static final String FRENCH_SPECIAL_CHARACTERS = "ÇçÀàÁáÂâÈèÉéÊêÔôÙùÛûÎîÏïŸÿ";
+	public static final String FRENCH_SPECIAL_CHARACTERS = "Ã‡Ã§Ã€Ã ÃÃ¡Ã‚Ã¢ÃˆÃ¨Ã‰Ã©ÃŠÃªÃ”Ã´Ã™Ã¹Ã›Ã»ÃÃ®ÃÃ¯ÂŸÃ¿";
 
-	public static final String STANDARD_SPECIAL_CHARACTERS = "€%$§#^°!?&{}[]:.;,|";
+	public static final String STANDARD_SPECIAL_CHARACTERS = "Â€%$Â§#^Â°!?&{}[]:.;,|";
 
 	public static final String MATHEMATICAL_SPECIAL_CHARACTERS = "+-~<>=/*";
 	
