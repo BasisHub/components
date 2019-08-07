e444288c6a85feb0b55d41cc85e33e1fd4d1f936
Christian Bauer <christian.b1998@web.de>
2019-08-07 14:33:24 +0200

    Changes to ResultSetExporterTest and JLibResultSetImporterTest; pom Guava dependecy

--- src/test/java/com/basiscomponents/db/ResultSetExporterTest.java
+++ src/test/java/com/basiscomponents/db/ResultSetExporterTest.java
@@ -1,7 +1,9 @@
 package com.basiscomponents.db;
 
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.OutputStream;
+import java.util.HashMap;
 
 import org.junit.jupiter.api.Test;
 
@@ -11,16 +13,46 @@ import com.basiscomponents.db.util.ResultSetProvider;
 public class ResultSetExporterTest {
 
 	/**
-	 * Simple test of the ResultSetExporter.
+	 * Simple test of the ResultSetExporter's Excel writing method
 	 * 
 	 * @throws Exception
 	 */
 	@Test
-	public void simpleResultSetExporterTest() throws Exception {
+	public void simpleResultSetExporterExcelTest() throws Exception {
 		ResultSet rs = ResultSetProvider.createDefaultResultSet();
 		OutputStream out = new FileOutputStream("./src/test/testExcelExports/test1.xlsx");
 		SheetConfiguration sheetConfig = new SheetConfiguration();
 		sheetConfig.addColumn("LISTFIELD", 300, true);
 		ResultSetExporter.writeXLSX(rs, out, true, false, "myLabel", null, sheetConfig);
+	}
+
+	/**
+	 * Simple test of the ResultSetExporter's Html writing method
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void simpleResultSetExporterHtmlTest() throws Exception {
+
+		ResultSet rs = new ResultSet();
+
+		DataRow dr = new DataRow();
+		dr.setFieldValue("feld1", "TEST1");
+		dr.setFieldValue("feld2", "TEST2");
+		rs.add(dr);
+
+		dr = new DataRow();
+		dr.setFieldValue("feld1", "TEST1");
+		dr.setFieldValue("feld3", "TEST3");
+		rs.add(dr);
+
+		HashMap<String, String> links = new HashMap<>();
+		links.put("feld1", "/rest/test/{feld1}");
+		links.put("feld2", "/rest/test/{feld1}/{feld2}");
+
+		FileWriter fw = new FileWriter("./src/test/testHTMLFiles/test.html");
+		ResultSetExporter.writeHTML(rs, fw, links);
+		fw.flush();
+		fw.close();
 	}
 }
--- src/test/java/com/basiscomponents/db/importer/JLibResultSetImporterTest.java
+++ src/test/java/com/basiscomponents/db/importer/JLibResultSetImporterTest.java
@@ -8,9 +8,10 @@ public class JLibResultSetImporterTest {
 
 	@Test
 	public void jlibImporterTest() throws IndexOutOfBoundsException, NoSuchFieldException, Exception {
+		System.setProperty("user.name", "admin");
 		String template = "CDNUMBER:C(6*=10):DESCRIPTION=CD_Inventory_Number:,TITLE:C(50*=10):DESCRIPTION=CD_Title:,ARTIST:C(50*=10):DESCRIPTION=Artist_Name:,LABEL:C(50*=10):DESCRIPTION=Recording_Label:,PLAYINGTIME:C(6*=10):DESCRIPTION=CD_Playing_Time:,RECORDINGTYPE:C(3*=10):DESCRIPTION=CD_Recording_Type:,MUSICTYPE:C(15*=10):DESCRIPTION=Type_of_Music:,BINLOCATION:C(10*=10):DESCRIPTION=Location_of_Bin_containing_the_CD:,NUMBEROFTRACKS:N(10):DESCRIPTION=Number_of_Tracks:,ONHAND:N(10):DESCRIPTION=Quantity_on_Hand:,COST:N(10):DESCRIPTION=Wholesale_cost_of_CD:,RETAIL:N(10):DESCRIPTION=Retail_Price_of_CD:";
 		JLibResultSetImporter jri = new JLibResultSetImporter();
-		jri.setFile("C:\\bbj\\demos\\cdstore\\data", template);
+		jri.setFile("C:\\bbj\\demos\\cdstore\\data\\cdstore", template);
 		ResultSet rs = jri.retrieve();
 		System.out.println(rs.toJson());
 	}
