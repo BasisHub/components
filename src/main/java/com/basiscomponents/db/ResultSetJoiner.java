d304d3558e3fbad6ab91f927874654b7f925bad6
Christian Bauer <christian.b1998@web.de>
2019-08-08 17:03:35 +0200

    <feat>[ResultSetJoiner]: added documentation

--- src/main/java/com/basiscomponents/db/ResultSetJoiner.java
+++ src/main/java/com/basiscomponents/db/ResultSetJoiner.java
@@ -7,14 +7,30 @@ import java.util.List;
 
 public class ResultSetJoiner {
 
+	/**
+	 * @See full parameter leftJoin method
+	 */
 	public static ResultSet leftJoin(ResultSet left, ResultSet right, String joinFieldName) throws ParseException {
 		return ResultSetJoiner.leftJoin(left, right, joinFieldName, null);
 	}
 
+	/**
+	 * Performs a Left-Join on two ResultSet.
+	 * 
+	 * @param left:             The left part of the join
+	 * @param right:            The right part of the join
+	 * @param joinFieldName:    The FieldName which should be used to join
+	 * @param targetFieldNames: The FieldNames which should be joined into the left
+	 *                          ResultSet
+	 * 
+	 * @return The left-joined ResultSet as an Result of the two input ResultSets
+	 * @throws ParseException
+	 */
 	public static ResultSet leftJoin(ResultSet left, ResultSet right, String joinFieldName,
 			List<String> targetFieldNames)
 			throws ParseException {
 		
+		// If the targeFieldNames are not specified, all fields will be joined
 		if (targetFieldNames == null || targetFieldNames.isEmpty()) {
 			targetFieldNames = right.get(0).getFieldNames();
 			targetFieldNames.remove(joinFieldName);
